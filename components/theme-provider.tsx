f722238fb5f2dffe4228a391d436eb0782c14895
AirlanggaFzn <nirrangga2002@gmail.com>
2025-09-29 05:09:25 +0700

    .

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -1,11 +1,16 @@
 'use client'
 
 import * as React from 'react'
-import {
-  ThemeProvider as NextThemesProvider,
-  type ThemeProviderProps,
-} from 'next-themes'
+import { ThemeProvider as NextThemesProvider, type ThemeProviderProps } from 'next-themes'
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>
+  return (
+    <NextThemesProvider
+      {...props}
+      attribute="class"       // biasanya dipakai untuk class-based theming
+      defaultTheme="system"   // theme default saat pertama load
+    >
+      {children}
+    </NextThemesProvider>
+  )
 }
